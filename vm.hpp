fb87e8e2fa54c7ef89eea016246ef26533c483e3
Addy <37238444+x-Eagle-x@users.noreply.github.com>
2021-10-10 22:47:05 +0200

    Update vm.hpp

--- vm.hpp
+++ vm.hpp
@@ -3,7 +3,7 @@
 
 #include "registers.hpp"
 
-#define PROGRAM_SIZE 2048
+#define PROGRAM_SIZE 4096
 #define MEMORY_SIZE 1024
 
 typedef unsigned char uint8_t;
@@ -13,11 +13,18 @@ typedef unsigned int uint32_t;
     #error "Not enough memory for registers."
 #endif
 
+enum
+{
+    PTR_NONE,
+    PTR_LEFT = 1,
+    PTR_RIGHT = 2
+};
+
 struct VM
 {
-    bool running = true, use_memory = false;
+    bool running = true;
 
-    uint8_t program[PROGRAM_SIZE] = {0};
+    uint8_t program[PROGRAM_SIZE] = {0}, lr_ptr = 0;
     uint32_t memory[MEMORY_SIZE] = {0}, operands[2] = {0};
 
     uint32_t &acc_a = memory[REG::A0], &acc_b = memory[REG::B0];
